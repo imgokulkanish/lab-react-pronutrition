16e349010ab6277d13040502afba4bf97227c699
Gokul Kanish <66904387+imgokulkanish@users.noreply.github.com>
2021-10-14 19:19:30 +0530

    completed

---
+++ pronutrition/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Foodbox from './components/FoodBox'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+  <div>
+          <Navbar />
+          <Foodbox />
+  </div>
+  );
+}
+
+export default App;
